0ac56bffb5bff25c485b0cf211e63b790d0b2b76
Limorinho <ujkanilinor@gmail.com>
2025-06-30 22:49:02 +0000

    fixed send button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,7 +3,7 @@
 
 <h1>Contact Us</h1>
 
-<section class="">
+<section class="max-w-xl mx-auto">
 	<div class="py-8 lg:py-16">
 		<p class="mx-auto text-center text-(--color-theme-1) md:text-lg">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
 		<form action="#" class="space-y-8 py-16">
@@ -19,7 +19,17 @@
 				<label for="message" class="block mb-2 font-bold">Your message</label>
 				<textarea id="message" rows="6" class="bg-sky-500/30 text-sky-600 border-sky-500 block w-full text-sm bg-gray-50 rounded-lg shadow-sm border focus:ring-primary-500" placeholder="Leave a comment..."></textarea>
 			</div>
-			<button type="submit" class="py-3 text-center text-white rounded-lg bg-sky-700 sm:w-fit hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
+			<div class="flex justify-center">
+			<button
+			type="submit"
+			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send message
+			<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" 
+				 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+			  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" 
+					stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+			</svg>
+		  </button>		  
+		</div>
 		</form>
 	</div>
   </section>
@@ -30,8 +40,5 @@
 		height: 100%;
 		display: flex;
 		flex-direction: column;
-	}
-	button {
-		align-items: center;
 	}
 </style>
